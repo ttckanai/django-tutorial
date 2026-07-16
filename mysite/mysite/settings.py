ac6b0fa7dd3644c7d59d40dd48a1a356afb01f55
kanai <s.kanai@ttc.ac.jp>
2026-07-16 18:54:21 +0900

    set staticroot

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -136,7 +136,7 @@ STATIC_URL = 'static/'
 # CSSなどの静的ファイルの設定を追記
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'mysite', 'staticfiles')
 
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
